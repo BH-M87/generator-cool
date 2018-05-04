df6301ee0af96f349f624de99b4a092b5a1a25fa
Xuheng Liu <liuxuheng1990@gmail.com>
2018-05-04 18:19:09 +0800

    feat: Add http.js

---
+++ generators/app/templates/src/common/http.js
@@ -0,0 +1,149 @@
+/* global location */
+import _ from 'lodash';
+import fetch from 'isomorphic-fetch';
+import history from 'common/history';
+
+export class Http {
+  defaultConfig = {
+    csrf: {
+      api: '/getCsrfToken',
+      timeout: 11 * 60 * 10000,
+      getToken: response => response.token
+    },
+    errorHook: error => {
+      throw error;
+    },
+    notLoginInErrorCode: 2001,
+    notLoginIn: () => {
+      /* eslint no-restricted-globals:0 */
+      // history.push({ pathname: routeConfig.login, search: `?redirectUrl=${location.pathname}` });
+      history.push({ pathname: '/login' });
+    }
+  };
+
+  checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.data = response;
+    throw error;
+  }
+
+  checkErrCode(dataObj) {
+    const { errCode, data, errMsg } = dataObj;
+    if (!errCode || errCode === 0) {
+      return;
+    }
+    if (errCode === this.defaultConfig.notLoginInErrorCode) {
+      this.defaultConfig.notLoginIn();
+    }
+
+    const error = new Error(errMsg);
+    error.code = errCode;
+    error.data = data;
+    throw error;
+  }
+
+  parseResult(data) {
+    this.checkErrCode(data);
+    return data && data.data;
+  }
+
+  async parseJSON(response) {
+    return response.json();
+  }
+
+  async processResult(response) {
+    let returnResponse = this.checkStatus(response);
+    returnResponse = await this.parseJSON(returnResponse);
+    return this.parseResult(returnResponse);
+  }
+
+  async request(url, init, headers = {}, config = { throwError: false }) {
+    try {
+      const options = _.assign(
+        {
+          credentials: 'include'
+        },
+        init
+      );
+      options.headers = Object.assign(
+        {
+          'x-requested-with': 'XMLHttpRequest'
+        },
+        options.headers || {},
+        headers || {}
+      );
+      let response = await fetch(url, options);
+      response = await this.processResult(response);
+      return response;
+    } catch (error) {
+      this.defaultConfig.errorHook(error);
+      if (config.throwError) throw error;
+      return null;
+    }
+  }
+
+  /**
+   * headers
+   * csrf
+   * host
+   * cqrs
+   **/
+  set config(config) {
+    this.defaultConfig = {
+      ...this.defaultConfig,
+      ...config
+    };
+  }
+
+  token = null;
+  async getCsrfToken() {
+    if (!this.token) {
+      let response = await this.get(this.defaultConfig.csrf.api);
+      if (this.defaultConfig.csrf.getToken) {
+        response = this.defaultConfig.csrf.getToken(response);
+      }
+      if (!response) {
+        throw new Error('CSRF Not EXIST!');
+      }
+      // response is a object with attribute token
+      this.token = response;
+      setTimeout(() => {
+        this.token = undefined;
+      }, this.defaultConfig.csrf.timeout || 11 * 60 * 1000);
+    }
+    return this.token;
+  }
+
+  async get(api, data = {}, headers = {}, config = {}) {
+    const query = _.isEmpty(data) ? '' : `json=${encodeURIComponent(JSON.stringify(data))}`;
+    return this.request(`${api}?${query}`, {}, headers, config);
+  }
+
+  async post(api, data = {}, customeHeaders = {}, config = {}) {
+    const token = await this.getCsrfToken();
+    const headers = {
+      'Content-Type': 'application/json',
+      'X-CSRF-TOKEN': token,
+      ...customeHeaders
+    };
+    const formBody = JSON.stringify(data);
+    return this.request(
+      api,
+      {
+        method: 'POST',
+        headers,
+        body: formBody
+      },
+      {},
+      config
+    );
+  }
+}
+
+const http = new Http();
+
+export default http;
