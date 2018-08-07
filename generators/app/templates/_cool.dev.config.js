621bac087e9b786fb15d0b9efc058e96c1050fb1
Xuheng Liu <liuxuheng1990@gmail.com>
2018-08-07 20:37:52 +0800

    feat: update proxy setting, add support for WebSocket proxy.

--- generators/app/templates/_cool.dev.config.js
+++ generators/app/templates/_cool.dev.config.js
@@ -1,18 +1,31 @@
+const TARGET = 'http://remoteHost:7001';
+
 module.exports = {
   devServer: {
     port: '8001',
-    //   proxy: {
-    //     '*': {
-    //       target: '',
-    //       changeOrigin: true,
-    //       secure: false,
-    //       bypass: req => {
-    //         if (req.headers['x-requested-with'] !== 'XMLHttpRequest') {
-    //           return req.originalUrl;
-    //         }
-    //         return false;
-    //       },
-    //     },
-    //   },
-    // },
+    proxy: {
+      // Rules refer to https://www.npmjs.com/package/glob
+      // /sockjs-node/** is used for webpack hot reload, do not proxy it
+      '!(/sockjs-node/**)': {
+        target: TARGET,
+        changeOrigin: true,
+        ws: true,
+        onProxyReqWs: proxyReq => {
+          proxyReq.setHeader('origin', TARGET);
+        },
+        secure: false,
+        bypass: req => {
+          const { headers, originalUrl } = req;
+          // only proxy for xhr and image request
+          if (
+            headers['x-requested-with'] !== 'XMLHttpRequest' &&
+            headers.accept.indexOf('image/') === -1
+          ) {
+            return originalUrl;
+          }
+          return false;
+        },
+      },
+    },
+  },
 };
