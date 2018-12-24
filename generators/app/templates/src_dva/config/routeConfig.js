68fbcb92f4245f6a6ec5f31ce4ac1b0da3713e14
Xuheng Liu <liuxuheng1990@gmail.com>
2018-12-24 17:38:09 +0800

    feat: update dependencies version and add login path in routeConfig.js

--- generators/app/templates/src_dva/config/routeConfig.js
+++ generators/app/templates/src_dva/config/routeConfig.js
@@ -1,4 +1,5 @@
 export default {
+  login: { path: '/login', role: ['common'] },
   home: { path: '/', role: ['common'] },
   testPage: { path: '/testPage', role: ['common'] },
   detail: { path: '/detail', role: ['common'] },
--- generators/app/templates/src_mobx/config/routeConfig.js
+++ generators/app/templates/src_mobx/config/routeConfig.js
@@ -1,4 +1,5 @@
 export default {
+  login: { path: '/login', role: ['common'] },
   home: { path: '/home', role: ['common'] },
   testPage: { path: '/testPage', role: ['common'] },
 };
