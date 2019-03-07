83d463276418ef59a665475cb5154873d80de9d7
Xuheng Liu <liuxuheng1990@gmail.com>
2019-03-07 20:23:37 +0800

    feat: Using lazy load in router to improve performance

--- generators/app/templates/src_dva/router.jsx
+++ generators/app/templates/src_dva/router.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import PropTypes from 'prop-types';
 import { Router, Route, Switch } from 'dva/router';
 import routeConfig from 'config/routeConfig';
-import IndexPage from 'routes/IndexPage';
-import DetailPage from 'routes/DetailPage';
+
+const IndexPage = lazy(() => import('routes/IndexPage'));
+const DetailPage = lazy(() => import('routes/DetailPage'));
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-      <Switch>
-        <Route path={routeConfig.home.path} exact>
-          <IndexPage />
-        </Route>
-        <Route path={routeConfig.detail.path} exact>
-          <DetailPage />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path={routeConfig.home.path} exact>
+            <IndexPage />
+          </Route>
+          <Route path={routeConfig.detail.path} exact>
+            <DetailPage />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
 
 RouterConfig.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  history: PropTypes.object,
+  history: PropTypes.object
 };
 
 export default RouterConfig;
--- generators/app/templates/src_mobx/routes/index.jsx
+++ generators/app/templates/src_mobx/routes/index.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router';
 import history from 'common/history';
-import TopBar from 'components/TopBar';
 import routeConfig from 'config/routeConfig';
+
+const TopBar = lazy(() => import('components/TopBar'));
 
 export default function RouterConfig() {
   return (
     <Router history={history}>
-      <Switch>
-        <Route path={routeConfig.home.path} exact>
-          <TopBar />
-        </Route>
-        <Route path={routeConfig.testPage.path} exact>
-          <TopBar />
-        </Route>
-        <Redirect to={routeConfig.home} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path={routeConfig.home.path} exact>
+            <TopBar />
+          </Route>
+          <Route path={routeConfig.testPage.path} exact>
+            <TopBar />
+          </Route>
+          <Redirect to={routeConfig.home} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
