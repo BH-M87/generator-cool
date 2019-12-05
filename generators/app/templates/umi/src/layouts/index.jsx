7fefddb19f5ee5fb526d7b0079da5dc5e88de48a
Xuheng Liu <liuxuheng1990@gmail.com>
2019-12-05 16:38:59 +0800

    feat: add componentDidCatch

--- generators/app/templates/umi/src/layouts/index.jsx
+++ generators/app/templates/umi/src/layouts/index.jsx
@@ -1,12 +1,44 @@
-import styles from './index.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { history } from 'cool-utils';
+import Error from 'components/Error';
 
-function BasicLayout(props) {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
-    </div>
-  );
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { errorMessage: undefined };
+  }
+
+  componentDidCatch(error, info) {
+    console.error('Layout Catched Error:', error, info);
+    this.setState({
+      errorMessage: error.message || 'Layout Catched Error with no message.',
+    });
+  }
+
+  render() {
+    const { errorMessage } = this.state;
+    const { children } = this.props;
+    if (errorMessage) {
+      return (
+        <Error
+          message={errorMessage}
+          returnButtonText="返回首页"
+          onReturn={() => {
+            history.push('/');
+            this.setState({
+              errorMessage: undefined,
+            });
+          }}
+        />
+      );
+    }
+    return children;
+  }
 }
 
-export default BasicLayout;
+Layout.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Layout;
